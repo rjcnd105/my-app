6958d1495a8d9aa48852a464922f73e7bfed2982
김회준 <rjcnd123@gmail.com>
2025-01-21 18:32:42 +0900

    add validate message

--- dutchpay_umbrella/apps/dutchpay/lib/dutchpay/chat/context.ex
+++ dutchpay_umbrella/apps/dutchpay/lib/dutchpay/chat/context.ex
@@ -2,11 +2,19 @@ defmodule Dutchpay.Chat do
   @moduledoc false
   import Ecto.Query
 
-  alias Dutchpay.Chat.Room.Schema
+  alias Dutchpay.Chat.Room
   alias Dutchpay.Repo
 
   @doc false
   def list_rooms do
-    Repo.all(from(Schema, order_by: [desc: :updated_at]))
+    Repo.all(from(Room.Schema, order_by: [desc: :updated_at]))
+  end
+
+  def create_room(attrs) do
+    %Room.Schema{}
+    # 제약 조건 검사
+    # 패턴 매칭을 통해 { :ok, ... } 일때는 insert되고 { :error, ... } 일때는 insert되지 않는다.
+    |> Room.Schema.changeset(attrs)
+    |> Repo.insert()
   end
 end
--- dutchpay_umbrella/apps/dutchpay/lib/dutchpay/chat/room/schema.ex
+++ dutchpay_umbrella/apps/dutchpay/lib/dutchpay/chat/room/schema.ex
@@ -12,9 +12,13 @@ defmodule Dutchpay.Chat.Room.Schema do
   end
 
   @doc false
+  # 제약 조건 추가
   def changeset(room, attrs) do
     room
     |> cast(attrs, [:name, :topic])
-    |> validate_required([:name, :topic])
+    |> validate_required(:name, message: "방 이름을 입력해주세요.")
+    |> validate_length(:name, min: 3, max: 80, message: "최소 3자, 최대 80자 입력가능합니다")
+    |> validate_format(:name, ~r/\A[ㄱ-ㅎA-z0-9-_]+\z/, message: "한글, 영문, 숫자, -, _만 포함할 수 있습니다.")
+    |> validate_length(:topic, max: 200, message: "최대 200자까지 입력 가능합니다.")
   end
 end
