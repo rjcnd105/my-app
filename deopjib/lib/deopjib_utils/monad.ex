19c000df8b9927e3150b7bd94f0c942f91341e3b
김회준 <rjcnd123@gmail.com>
2025-03-15 16:46:34 +0900

    update monad utils

--- deopjib/lib/deopjib_utils/monad.ex
+++ deopjib/lib/deopjib_utils/monad.ex
@@ -1,24 +1,44 @@
 defmodule Monad.Result do
-  @type result_t(a) :: ok_t(a) | error_t(a)
+  @type result_t(value, error) :: ok_t(value) | error_t(error)
   @type ok_t(a) :: {:ok, a}
   @type error_t(a) :: {:error, a}
 
+  @spec map(result_t(v, e), (v -> b)) :: result_t(b, e) when v: term(), b: term(), e: term()
   def map({:ok, val}, f), do: {:ok, f.(val)}
   def map({:error, _val} = err, _f), do: err
 
-  @spec match(result_t(a), %{ok: (a -> b), error: (a -> c)}) :: b | c
-        when a: term(), b: term(), c: term()
+  @spec map_err(result_t(any(), a), (a -> b)) :: result_t(any(), b) when a: term(), b: term()
+  def map_err({:error, value}, f), do: {:error, f.(value)}
+  def map_err({:ok, _} = result, _f), do: result
+
+  @spec flat_map(result_t(a, e), (a -> result_t(b, e))) :: result_t(b, e)
+        when a: term(), b: term(), e: term()
+  def flat_map({:ok, val}, f), do: f.(val)
+  def flat_map({:error, _val} = err, _f), do: err
+
+  @spec match(result_t(v, e), %{ok: (v -> b), error: (e -> c)}) :: b | c
+        when v: term(), e: term(), b: term(), c: term()
 
   def match({:ok, val}, %{ok: ok_fn, error: _err_fn}), do: ok_fn.(val)
   def match({:error, val}, %{ok: _ok_fn, error: err_fn}), do: err_fn.(val)
 
+  @spec ok(a) :: ok_t(a) when a: term()
   def ok(val), do: {:ok, val}
 
+  @spec ok?(result_t(any(), any())) :: boolean()
   def ok?({:ok, _val}), do: true
   def ok?({:error, _val}), do: false
 
+  @spec err(a) :: error_t(a) when a: term()
   def err(val), do: {:error, val}
 
+  @spec err?(result_t(any(), any())) :: boolean()
   def err?({:error, _val}), do: true
   def err?({:ok, _val}), do: false
+
+  @spec fold([result_t(v, e)]) :: result_t([v], e) when v: term(), e: term()
+  def fold(list), do: fold(list, [])
+  defp fold([{:ok, v} | tail], acc), do: fold(tail, [v | acc])
+  defp fold([{:error, v} | _tail], _acc), do: {:error, v}
+  defp fold([], acc), do: {:ok, Enum.reverse(acc)}
 end
