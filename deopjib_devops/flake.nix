0e6c09b36ff28b26d74cc0573d64dd8ab10c9d4c
김회준 <rjcnd123@gmail.com>
2025-09-02 14:04:28 +0900

    un override

--- deopjib_devops/flake.nix
+++ deopjib_devops/flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
   };
