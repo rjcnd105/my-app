493a9844812a6ab5b455dfdc4a2b2c2c9d032509
김회준 <rjcnd123@gmail.com>
2025-09-16 10:12:08 +0900

    restore mix

---
+++ deopjib/mix.exs
@@ -0,0 +1,109 @@
+defmodule Deopjib.MixProject do
+  use Mix.Project
+  @assets_path Path.expand("./assets", __DIR__)
+
+  def project do
+    [
+      app: :deopjib,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      consolidate_protocols: Mix.env() != :dev,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {Deopjib.Application, []},
+      extra_applications: [:logger, :runtime_tools, :os_mon]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:usage_rules, "~> 0.1", only: [:dev]},
+      {:ash_admin, "~> 0.13"},
+      {:ash_cloak, "~> 0.1"},
+      {:cloak, "~> 1.1"},
+      {:ash_events, "~> 0.4.0"},
+      {:ash_json_api, "~> 1.0", override: true},
+      {:ex_cldr, "~> 2.40"},
+      {:ex_cldr_numbers, "~> 2.33"},
+      {:puid, "~> 2.3"},
+      {:ash_state_machine, "~> 0.2"},
+      {:json_xema, "~> 0.6"},
+      {:ash_typescript, "~> 0.1.2"},
+      # {:phoenix_sync, "~> 0.5.0"},
+      {:open_api_spex, "~> 3.21"},
+      {:ash_authentication_phoenix, "~> 2.0"},
+      {:picosat_elixir, "~> 0.2.3"},
+      {:ash_authentication, "~> 4.0"},
+      {:ash_postgres, "~> 2.0"},
+      {:ash_phoenix, "~> 2.0"},
+      {:sourceror, "~> 1.10", only: [:dev, :test]},
+      {:ash, "~> 3.0"},
+      {:igniter, "~> 0.6", only: [:dev, :test]},
+      {:phoenix, "~> 1.7.20"},
+      {:phoenix_ecto, "~> 4.5"},
+      {:ecto_sql, "~> 3.10"},
+      {:postgrex, ">= 0.0.0"},
+      {:spark, "~> 2.2"},
+      {:phoenix_html, "~> 4.1"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 1.1.0"},
+      {:live_debugger, "~> 0.1.0", only: :dev},
+      {:floki, ">= 0.37.0"},
+      {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:heroicons,
+       github: "tailwindlabs/heroicons",
+       tag: "v2.2.0",
+       sparse: "optimized",
+       app: false,
+       compile: false,
+       depth: 1},
+      {:swoosh, "~> 1.5"},
+      {:finch, "~> 0.13"},
+      {:telemetry_metrics, "~> 1.0"},
+      {:telemetry_poller, "~> 1.0"},
+      {:gettext, "~> 0.26"},
+      {:dns_cluster, "~> 0.2"},
+      {:bandit, "~> 1.5"}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      setup: ["deps.get", "ash.setup", "assets.setup", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ash.setup --quiet", "test"],
+      "assets.setup": ["cmd bun install --only-missing"],
+      "assets.deploy": [
+        "cmd --cd #{@assets_path} bun js --deploy",
+        "cmd --cd #{@assets_path} bun tailwind --minify",
+        "phx.digest"
+      ],
+      "gen.openapi": ["openapi.spec.json --spec DeopjibWeb.AshJsonApiRouter"],
+      "phx.routes": ["phx.routes", "ash_authentication.phoenix.routes", "ash_json_api.routes"]
+    ]
+  end
+end
